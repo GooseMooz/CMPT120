cd1506fdc1e887b1dd4972f21ce5236af06b6e7c
GooseMooz <goosev.goose@gmail.com>
2023-01-10 19:59:00 -0800

    bibs

--- Week 1/main.py
+++ Week 1/main.py
@@ -1,2 +1,10 @@
 print('Hello World')
 print(3 + 5)
+message = 'Hello'
+print(message)
+pi = True
+print(pi)
+print(type(pi))
+print(type(message))
+a = int(input('Enter a number \n'))
+print(2 * a)
