d994391f31e807e969633670aeeb2f2df8142f39
GooseMooz <goosev.goose@gmail.com>
2023-01-27 15:23:30 -0800

    Week 2 ඞ

--- Week 3/Labwork/LabworkExtra.py
+++ Week 3/Labwork/LabworkExtra.py
@@ -4,10 +4,12 @@ a = 1.0
 b = 2.0
 f1 = a * a - 2.0
 f2 = b * b - 2.0
-if a * b < 0:
+while f1 * f2 < 0:
     ans = (a + b) / 2
+    print(ans)
     b = ans
     if cnt % 2 == 0:
         b = ans
     if cnt % 2 == 1:
         a = ans
+print(ans)
