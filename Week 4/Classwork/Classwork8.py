56c9e561ff69be1d6e43f85fb75b50354e12eae6
GooseMooz <goosev.goose@gmail.com>
2023-01-31 20:35:15 -0800

    Week 4

---
+++ Week 4/Classwork/Classwork8.py
@@ -0,0 +1,13 @@
+import math
+
+cnt = 0
+num = 1
+while cnt < 3:
+    svm = 0
+    num += 1
+    for i in range(1, int(num / 2) + 2):
+        if num % i == 0:
+            svm += i
+    if svm == num:
+        cnt += 1
+        print(num)
